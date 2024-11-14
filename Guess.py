a1a02e0818dcc1f42c5e8f96540f5cd030b6badd
Ganzadonatien <niyocroirealine@gmail.com>
2024-11-14 18:54:57 +0200

    Initial Commit

---
+++ Guess.py
@@ -0,0 +1,29 @@
+import random
+top_range=input("Enter a maximum number : ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print("Enteer number greater than zero next time")
+        quit()
+else:
+    print("please type in a number next time:")
+    quit()
+
+random_number=random.randint(0,top_range)
+guess=0
+while True:
+    guess += 1
+    user_guess=int(input("Guess a number "))
+    if user_guess==random_number :
+        print("you got it!")
+        break
+    elif(user_guess>random_number):
+        print("You were above the number!")
+    else:
+        print("You were Below the number!")
+print('You got it in',guess,'guesses')
+
+
+
